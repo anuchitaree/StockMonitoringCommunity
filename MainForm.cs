4109663a8f48fd867330052ad3c47425a280cf94
diatanuchit <anuchit.aree.a9f@ap.denso.com>
2025-12-17 15:43:59 +0700

    initial

---
+++ MainForm.cs
@@ -0,0 +1,10 @@
+namespace StockMonitoringCommunity
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
